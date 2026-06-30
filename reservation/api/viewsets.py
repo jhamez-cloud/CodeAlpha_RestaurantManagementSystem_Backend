a1e03814f9fb813a7f8c7ceb6d071dd122aff576
jhamez-cloud <jameskekeli12@gmail.com>
2026-06-30 03:31:00 +0000

    update: added permission classes

--- reservation/api/viewsets.py
+++ reservation/api/viewsets.py
@@ -1,7 +1,9 @@
 from config.viewsets import StandardViewset
+from rest_framework.permissions import AllowAny
 from reservation.serializers import ReservationSerializer
 from reservation.models import Reservation
 
 class ReservationViewset(StandardViewset):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+    permission_classes = (AllowAny,)
