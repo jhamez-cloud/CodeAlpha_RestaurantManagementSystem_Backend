d22dc7fce0754a1df4771cc53d1f89c263d8614e
jhamez-cloud <jameskekeli12@gmail.com>
2026-06-30 03:30:41 +0000

    update: added permission classes

--- inventory/api/viewsets.py
+++ inventory/api/viewsets.py
@@ -1,7 +1,9 @@
 from config.viewsets import StandardViewset
+from rest_framework.permissions import AllowAny
 from inventory.serializers import InventorySerializer
 from inventory.models import Inventory
 
 class InventoryViewset(StandardViewset):
     queryset = Inventory.objects.all()
     serializer_class = InventorySerializer
+    permission_classes = (AllowAny,)
