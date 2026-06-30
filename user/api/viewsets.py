a74da4252a3903c0e88d9379570759c4ca443901
jhamez-cloud <jameskekeli12@gmail.com>
2026-06-30 03:31:07 +0000

    update: added permission classes

--- user/api/viewsets.py
+++ user/api/viewsets.py
@@ -1,7 +1,9 @@
 from config.viewsets import StandardViewset
+from rest_framework.permissions import AllowAny
 from user.models import User
 from user.serializers import UserSerializer
 
 class UserViewset(StandardViewset):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
